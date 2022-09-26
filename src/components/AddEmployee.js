8b37eb805e6835cb445a8700eb57e56d6850dbc7
Samuel Slosser <samuel.slosser@cgm.com>
2022-09-26 14:08:13 +0000

    Added disabled add button Added employeeRecord obj added handle function for input changes

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,34 +1,79 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react';
+/**
+ * Table data component to add new employee
+ * @param {func} onSave - callback when a new employee save button clicked
+ * @returns {JSX} AddEmployee
+ */
+const AddEmployee = ({ onSave }) => {
+  const [employeeRecord, setEmployeeRecord] = useState({
+    name: '',
+    position: '',
+    salary: ''
+  });
 
-const AddEmployee = () => {
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+    setEmployeeRecord((previousState) => ({
+      ...previousState,
+      [name]: value
+    }));
+  };
+  const handleSave = () => {
+    // save record
+    onSave(employeeRecord);
+    // reset employee record
+    setEmployeeRecord({
+      name: '',
+      position: '',
+      salary: ''
+    });
+  };
   return (
     <Fragment>
       <td className="pl-30">
-        <input data-testid="new-employee-name-input" placeholder="Enter Name" />
-      </td>
-      <td className="pl-20">
         <input
-          data-testid="new-employee-position-input"
-          placeholder="Enter Position"
+          aria-label="New Employee Name Input"
+          name="name"
+          data-testid="new-employee-name-input"
+          placeholder="Enter Name"
+          value={employeeRecord.name}
+          onChange={(event) => handleInput(event)}
         />
       </td>
       <td className="pl-20">
         <input
+          aria-label="New Employee Position Input"
+          name="position"
+          data-testid="new-employee-position-input"
+          placeholder="Enter Position"
+          value={employeeRecord.position}
+          onChange={(event) => handleInput(event)}
+        />
+      </td>
+      <td className="pl-20">
+        <input
+          aria-label="New Employee Salary Input"
+          name="salary"
           data-testid="new-employee-salary-input"
           type="number"
           placeholder="Enter Salary"
+          value={employeeRecord.salary}
+          onChange={(event) => handleInput(event)}
         />
       </td>
       <td className="pl-20">
         <button
+          aria-label="Add New Employee Button"
           data-testid="add-new-employee-button"
           className="x-small w-75 ma-0 px-25"
+          disabled={Object.values(employeeRecord).includes('')}
+          onClick={handleSave}
         >
           Add
         </button>
       </td>
     </Fragment>
-  )
-}
+  );
+};
 
-export default AddEmployee
+export default AddEmployee;
